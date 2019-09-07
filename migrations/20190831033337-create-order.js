df3efe96eee78cd600f1ce3670a552f0c718bab4
dyah <dyahnrchls@gmail.com>
2019-09-07 22:31:02 +0700

    modify relation

--- migrations/20190831033337-create-order.js
+++ migrations/20190831033337-create-order.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'menu',
+          model: 'menus',
           key: 'id'
         },
         onUpdate: 'cascade',
